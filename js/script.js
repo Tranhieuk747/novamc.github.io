a764a52eb47918e54535768dfefc6ad6147894d1
Hano47 <156065410+Tranhieuk747@users.noreply.github.com>
2024-12-24 13:44:25 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const config = {
         serverLogoImageFileName: "logo.png", /*This is a file name for logo in /images/ (If you upload new logo with other name, you must change this value)*/
         serverName: "NovaMC", /*Server name*/
         serverIp: "novamc.asia", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
-        discordServerID: "862882805301379092" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
+        discordServerID: "1320310264171855973" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
     },
 
     /*Admin-Team
